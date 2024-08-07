b2403adebe583b0680e6cd4a0e9c25a45296db4b
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2024-08-07 05:37:07 +0100

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contain `concat` function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """takes in two str parameters and concaenate them"""
+    return str1 + str2
