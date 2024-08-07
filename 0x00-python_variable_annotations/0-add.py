5db46659cda99f5912178afe246051b4142eb626
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2024-08-07 05:34:45 +0100

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module contain `add` function"""
+
+
+def add(a: float, b: float) -> float:
+    """takes in two parameters each is float
+    returns their sum in float
+    """
+    return a + b
